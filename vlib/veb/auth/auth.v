f3909e47705bae46dc0ed2b75c4be5ada1923266
Sky Johnson <skylearj@gmail.com>
2024-06-22 17:59:40 -0500

    add token test

--- vlib/veb/auth/auth.v
+++ vlib/veb/auth/auth.v
@@ -8,13 +8,13 @@ import crypto.bcrypt
 
 // Strategies for password hashing.
 // - bcrypt - The default strategy. Uses a sensible default cost.
-enum Strategy as u8 {
+pub enum Strategy as u8 {
 	bcrypt
 }
 
 pub struct Auth[T] {
-	db T
-	strategy u8
+	db       T
+	strategy Strategy
 }
 
 @[table: 'tokens']
@@ -28,43 +28,39 @@ pub:
 
 // Initialize an instance of Auth with your database driver of choice.
 // This will create the 'tokens' table with the 'Token' struct.
-// 
-// You can also optionally define a password hashing strategy from the 
+//
+// You can also optionally define a password hashing strategy from the
 // auth.Strategy enum; this defaults to bcrypt (0).
-pub fn new[T](db T, strategy ?u8) Auth[T] {
+pub fn new[T](db T, strategy Strategy) Auth[T] {
 	sql db {
 		create table Token
 	} or { eprintln('veb.auth: failed to create table Token') }
 
-	if strategy == none {
-		strategy = auth.Strategy.bcrypt
-	}
-
-	return Auth[T] {
+	return Auth[T]{
 		db: db
 		strategy: strategy
 	}
 }
 
 // Insert an authentication token with the user's ID.
-pub fn (mut app Auth[T]) add_token(user_id int) !string {
+pub fn (app Auth[T]) add_token(user_id int) !Token {
 	mut uuid := rand.uuid_v4()
 
 	token := Token{
 		user_id: user_id
-		value: uuid
+		token: uuid
 	}
 
 	sql app.db {
 		insert token into Token
 	}!
 
-	return uuid
+	return token
 }
 
 // Find a user's token by their user ID.
-pub fn (app &Auth[T]) find_token_by_id(user_id int) ?Token {
-	tokens := sql app.db {
+pub fn (a Auth[T]) find_token_by_id(user_id int) ?Token {
+	tokens := sql a.db {
 		select from Token where user_id == user_id limit 1
 	} or { []Token{} }
 
@@ -76,24 +72,25 @@ pub fn (app &Auth[T]) find_token_by_id(user_id int) ?Token {
 }
 
 // Delete all tokens associated with the user ID.
-pub fn (mut app Auth[T]) delete_tokens(user_id int) ! {
-	sql app.db {
+pub fn (a Auth[T]) delete_tokens(user_id int) ! {
+	sql a.db {
 		delete from Token where user_id == user_id
 	}!
 }
 
 // Hash a password using the chosen hashing strategy.
-fn (mut a Auth[T]) hash_password(password string) !string {
+pub fn (a Auth[T]) hash_password(password string) !string {
 	return match a.strategy {
-		0 { bcrypt.generate_from_password(password.bytes(), 15)! }
-		else { bcrypt.generate_from_password(password.bytes(), 15)! }
+		.bcrypt { bcrypt.generate_from_password(password.bytes(), 10)! }
 	}
 }
 
 // Check a hashed password using the chosen hashing strategy.
-fn (mut a Auth[T]) check_password(password string, hash string) bool {
-	return match a.strategy {
-		0 { bcrypt.compare_hash_and_password(password.bytes(), hash.bytes())! }
-		else { bcrypt.compare_hash_and_password(password.bytes(), hash.bytes())! }
+pub fn (a Auth[T]) check_password(password string, hash string) !bool {
+	match a.strategy {
+		.bcrypt {
+			bcrypt.compare_hash_and_password(password.bytes(), hash.bytes())!
+			return true
+		}
 	}
 }
--- vlib/veb/auth/auth_test.v
+++ vlib/veb/auth/auth_test.v
@@ -4,10 +4,29 @@ import db.sqlite
 fn test_password_hash() {
 	db := sqlite.connect(':memory:') or { panic(err) }
 
-	auth := vauth.new(db)
+	auth := vauth.new(db, vauth.Strategy.bcrypt)
 
 	pass := 'test-Passw0rd-1234!@#'
 	hash := auth.hash_password(pass)!
 
 	assert auth.check_password(pass, hash)!
+}
+
+fn test_token() {
+	db := sqlite.connect(':memory:') or { panic(err) }
+
+	auth := vauth.new(db, vauth.Strategy.bcrypt)
+
+	user_id := 1
+	token := auth.add_token(user_id)!
+
+	assert token.token.len > 0
+
+	tok := auth.find_token_by_id(user_id)?
+
+	assert tok.token == token.token
+
+	auth.delete_tokens(user_id) or { panic(err) }
+
+	assert auth.find_token_by_id(user_id) == none
 }
